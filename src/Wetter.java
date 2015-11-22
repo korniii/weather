35fd44b49bf7b6f57ca40db98e8f48493a958056
korniii <moritz.kornmeier@googlemail.com>
2015-11-22 12:28:05 +0100

    Initial commit

---
+++ src/Wetter.java
@@ -0,0 +1,29 @@
+/**
+ * Created by Moritz on 22.11.2015.
+ */
+public class Wetter {
+
+    String name;
+    int PLZ;
+    int temp;
+    String date;
+
+    public Wetter(String name, int PLZ, int temp, String date) {
+        this.name = name;
+        this.PLZ = PLZ;
+        this.temp = temp;
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPLZ() {
+        return PLZ;
+    }
+
+    public int getTemp() {
+        return temp;
+    }
+}
