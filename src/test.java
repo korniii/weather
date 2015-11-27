e70623974deecd4e0dcd1b7066c5b1bb456a51a9
Adrian <adrian.reh1304@gmail.com>
2015-11-27 14:50:57 +0100

    Es läuft!!!!!

--- src/test.java
+++ src/test.java
@@ -9,9 +9,8 @@ public class test {
     	//Gross-/Kleinschreibung ist bei den Städtennamen wichtig
     	
     	//for Schleife über alle Städte abhier beginnen
-    	String code = test.getCityCode("79539");
+    	String code = test.getCityCode("76133");
 
-    	System.out.println("Get City Code:");
         WeatherRequester http = new WeatherRequester();
 
         System.out.println("Get Weather:");
